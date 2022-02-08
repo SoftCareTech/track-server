e31796b2732fd928d24b09f1a9d19855829d9faa
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-02-08 22:21:05 +0100

    readme and refactor

---
+++ Readme.md
@@ -0,0 +1,12 @@
+##  Backend Server for Track project
+# Thing to learn in this project
+- Express 
+- Mongodb 
+- Authetication with Token(bcrpt and jsonwebtoken)
+- Mongoose
+
+# Installation and starting server
+``` 
+npm i
+npm run dev
+```
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const localUrl = 'mongodb://localhost:27017/track-movement-db'
-const mongoURL = "mongodb+srv://raph-ray:passwordnew@cluster0.mr4jc.mongodb.net/track-db?retryWrites=true&w=majority"
 const authRoute = require('./routes/authRoutes')
 const trackRoute = require('./routes/trackRoutes')
 const requireAuth = require('./middlewares/requireAuth')
@@ -12,9 +11,11 @@ const requireAuth = require('./middlewares/requireAuth')
 
 
 const app = express()
+const cors = require('cors');
+app.use(cors());
 app.use(bodyParser.json())
 app.use(authRoute)
-app.use(trackRoute)
+
 try {
     mongoose.connect(localUrl)  /// v6 options is not needed 
     mongoose.connection.on('connected', () => {
@@ -26,11 +27,18 @@ try {
 } catch (e) {
     console.error("Error conneting db too much", err)
 }
+app.get('/p', (req, res) => {
+    res.send("Hi   get P")
 
+})
+app.post('/p', (req, res) => {
+    res.send("Hi   Post  P")
+})
 app.get('/', requireAuth, (req, res) => {
     res.send("Hi  " + req.user.email)
 
 })
+app.use(trackRoute)
 app.listen(3000, () => {
     console.log("server started")
 })
